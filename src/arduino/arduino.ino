9f171c2c863e8800ff4cd411f8a953a14d6c74b2
Caxapok <caxapok.js@yandex.ru>
2023-10-10 10:53:50 +0300

    init

---
+++ src/arduino/arduino.ino
@@ -0,0 +1,54 @@
+// Include
+#include "Arduino.h" // basic library
+#include "reset.h" // rescure reset
+#include "joystick.h" // joystick library
+#include "motors.h" // motors driver library
+#include "manipulator.h" // manipulator library
+#include "rpi.h" // rpi library (no code)
+// #include "gyro.h" // gyto library (no code)
+//#include "automotive.h" // automatisation library
+
+// SETUP
+void setup(){
+    setupRescure(); // setup sercure reset pin
+
+    // debug
+    // setup serial
+    Serial.begin(115200);
+    delay(10);
+
+    // setup components
+    // firmata serail 57600 baud
+    setupJoystick(); // joystick
+
+    setupMotors(); // motors: setup drivers
+    Serial.println("[+] Motors has been configurated!!");
+
+    setupMan(); // manipulator
+    Serial.println("[+] Manipulator has been configurated!!");
+    // setupGyro(); // gyro & acceleration
+
+}
+
+// LOOP
+
+void loop(){
+    // debug
+    // testMan();
+    // calibrateSticks();
+
+    // main
+    // read joystic
+    readTumblers(); // tumblers
+    readSticks(); // sticks
+    readJoyButton(); // button
+
+    // control robot components
+    controlMotors(); // motors
+    controlMan(); // manipulator
+
+    // automotive
+    //checkAutomotive(); // check start for automotive
+
+}
+
