0b21e1b6e8563908a6e6031edddb879cb3479fb7
Caxapok <caxapok.js@yandex.ru>
2023-10-10 19:30:07 +0300

    camswitch

--- src/arduino/arduino.ino
+++ src/arduino/arduino.ino
@@ -6,7 +6,10 @@
 #include "manipulator.h" // manipulator library
 #include "rpi.h" // rpi library (no code)
 // #include "gyro.h" // gyto library (no code)
-//#include "automotive.h" // automatisation library
+#include "automotive.h" // automatisation library
+
+#define CAM_SWITCHER_PORT 13
+bool switchCameraStat = false;
 
 // SETUP
 void setup(){
@@ -37,18 +40,29 @@ void loop(){
     // testMan();
     // calibrateSticks();
 
+    // TODO: camera flip button
     // main
     // read joystic
-    readTumblers(); // tumblers
-    readSticks(); // sticks
     readJoyButton(); // button
+    readTumblers(); // tumblers
+    if (buttonLeft){ //stop motors if button pressed
+        leftStick = rightStick = stickRange[2];
+        controlMotors(); // motors
+        if (midTumbler > tumblerRange[1]) { // switch camera if mid tumbler pressed to low
+            switchCameraStat = !switchCameraStat;
+            digitalWrite(CAM_SWITCHER_PORT, switchCameraStat);
+        }
+        return;
+    }
+    readSticks(); // sticks
+    
 
     // control robot components
     controlMotors(); // motors
     controlMan(); // manipulator
+    
 
     // automotive
     //checkAutomotive(); // check start for automotive
-
 }
 
