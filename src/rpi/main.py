8c8b8944c4b0efdca8d61d854a04c6e117294b9a
Caxapok <caxapok.js@yandex.ru>
2024-10-26 02:51:16 +0500

    test

--- src/rpi/main.py
+++ src/rpi/main.py
@@ -11,7 +11,7 @@ while True:
     robot = Robot('/dev/ttyUSB0')  # with motors rasbberry pi
 
     # motors ports declaration
-    robot.setup_motors((3, 7, 5), (22, 24, 6), max_power=0.2, k=7)
+    robot.setup_motors((3, 7, 5), (22, 24, 12), max_power=0.2, k=7)
     robot.setup_camera(0)
     if int(sys.argv[1]) < 0:
         robot.camera.set_line_params(dt=0, work_pos=400, work_width=245, work_height=20, blur=13)
